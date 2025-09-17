45f68584e2f83294ef4d9692bf3e7d93f82cd48a
Adnan Sajid <adnansajid877@gmail.com>
2025-09-17 16:59:46 +0500

    Updated Hero & About Us section

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ function toggleMenu() {
   const navLinks = document.getElementById("navLinks");
   navLinks.classList.toggle("active");
 }
+
+const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.getElementById("navLinks");
+
+  // Toggle menu on hamburger click
+  hamburger.addEventListener("click", (e) => {
+    e.stopPropagation(); // prevent bubbling
+    navLinks.classList.toggle("active");
+  });
+
+  // Auto close when clicking outside
+  document.addEventListener("click", (e) => {
+    if (!navLinks.contains(e.target) && !hamburger.contains(e.target)) {
+      navLinks.classList.remove("active");
+    }
+  });
+
+  // Auto close when a link inside menu is clicked
+  document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("active");
+    });
+  });
